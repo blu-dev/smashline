c64f1a9d3546d2cd7d0e976f8b4e175a932aa876
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-12-23 14:38:30 -0500

    work?

--- src/lib.rs
+++ src/lib.rs
@@ -108,14 +108,14 @@ extern "Rust" {
 
     pub fn replace_fighter_frame(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: FighterFrame);
     pub fn replace_weapon_frame(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: AgentFrame);
-    pub fn replace_agent_frame_main(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: AgentFrame);
+    pub fn replace_agent_frame_main(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: FighterFrame);
 
     pub fn add_fighter_reset_callback(callback: FighterReset);
     pub fn add_agent_reset_callback(callback: AgentReset);
 
     pub fn add_fighter_frame_callback(callback: FighterFrameCallback);
     pub fn add_weapon_frame_callback(callback: AgentFrameCallback);
-    pub fn add_agent_frame_main_callback(callback: AgentFrameCallback);
+    pub fn add_agent_frame_main_callback(callback: FighterFrameCallback);
 
     pub fn add_fighter_init_callback(callback: FighterInit);
     pub fn add_agent_init_callback(callback: AgentInit);
