edea1c7f024c8aef2ea24c33a15bd6b7e702085b
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-12-26 01:01:20 -0500

    my b

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use smash::lua2cpp::*;
 
 pub use smashline_macro::*;
 
-use syn::Token;
-
 type FighterFrame = extern "C" fn(&mut L2CFighterCommon) -> L2CValue;
 type AgentFrame = extern "C" fn(&mut L2CFighterBase) -> L2CValue;
 type FighterFrameCallback = fn(&mut L2CFighterCommon);
@@ -110,14 +108,14 @@ extern "Rust" {
 
     pub fn replace_fighter_frame(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: FighterFrame);
     pub fn replace_weapon_frame(agent: LuaConstant, original: Option<&'static mut *const extern "C" fn()>, replacement: AgentFrame);
-    pub fn replace_agent_frame_main(agent: LuaConstant, is_fighter: bool, original: Option<&'static mut *const extern "C" fn()>, replacement: syn::Ident);
+    pub fn replace_agent_frame_main(agent: LuaConstant, is_fighter: bool, original: Option<&'static mut *const extern "C" fn()>, replacement: extern "C" fn());
 
     pub fn add_fighter_reset_callback(callback: FighterReset);
     pub fn add_agent_reset_callback(callback: AgentReset);
 
     pub fn add_fighter_frame_callback(callback: FighterFrameCallback);
     pub fn add_weapon_frame_callback(callback: AgentFrameCallback);
-    pub fn add_agent_frame_main_callback(callback: syn::Ident);
+    pub fn add_agent_frame_main_callback(callback: fn());
 
     pub fn add_fighter_init_callback(callback: FighterInit);
     pub fn add_agent_init_callback(callback: AgentInit);
